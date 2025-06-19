3f4cba48b72c0e684f71b5844a6314fad9467643
PC2502 <zxcv530@naver.com>
2025-06-19 13:02:06 +0900

    refactor: api에서 URLSearchParams를 사용하도록 변경

--- src/api.jsx
+++ src/api.jsx
@@ -6,13 +6,22 @@ export const getProducts = async ({
   pageSize = 4,
   keyword = "",
 }) => {
-  const query = `orderBy=${orderBy}&page=${currentPage}&pageSize=${pageSize}${
-    keyword ? `&keyword=${keyword}` : ""
-  }`;
-  const response = await fetch(`${BASE_URL}products?${query}`);
+  const params = new URLSearchParams({
+    orderBy,
+    page: currentPage,
+    pageSize,
+  });
+
+  if (keyword) {
+    params.append("keyword", keyword);
+  }
+
+  const response = await fetch(`${BASE_URL}products?${params.toString()}`);
+
   if (!response.ok) {
     throw new Error("항목을 불러오는데 실패했습니다.");
   }
+
   const body = await response.json();
   return body;
 };
