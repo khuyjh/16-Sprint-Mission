a0ffbe4bf93105475c873b097087f2f623df4e3e
PC2502 <zxcv530@naver.com>
2025-07-07 14:56:00 +0900

    fix: 함수 내 불필요한 console.log 삭제

--- src/utils/calcTimeFormNow.ts
+++ src/utils/calcTimeFormNow.ts
@@ -3,17 +3,11 @@ const calcTimeFromNow = (targetDate: string) => {
   const timestampTarget = new Date(targetDate).getTime();
 
   const passedSecond = Math.floor((now - timestampTarget) / 1000);
-  console.log(passedSecond);
   const passedMinute = Math.floor(passedSecond / 60);
-  console.log(passedMinute);
   const passedHour = Math.floor(passedMinute / 60);
-  console.log(passedHour);
   const passedDay = Math.floor(passedHour / 24);
-  console.log(passedDay);
   const passedMonth = Math.floor(passedDay / 30.4375);
-  console.log(passedMonth);
   const passedYear = Math.floor(passedMonth / 12);
-  console.log(passedYear);
 
   if (passedYear > 0) {
     return `${passedYear}년 전`;
