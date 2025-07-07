d885dc4b641a8bcae9b46a8dafbc08777fc3a927
PC2502 <zxcv530@naver.com>
2025-07-07 13:45:12 +0900

    fix: 날짜 변환 함수 수정

--- src/utils/calcTimeFormNow.ts
+++ src/utils/calcTimeFormNow.ts
@@ -1,19 +1,19 @@
 const calcTimeFromNow = (targetDate: string) => {
-  const now = new Date().toISOString();
-  const timestampTarget = new Date(targetDate).toISOString();
+  const now = Date.now();
+  const timestampTarget = new Date(targetDate).getTime();
 
-  const passedYear =
-    Number(now.slice(0, 4)) - Number(timestampTarget.slice(0, 4));
-  const passedMonth =
-    Number(now.slice(5, 7)) - Number(timestampTarget.slice(5, 7));
-  const passedDay =
-    Number(now.slice(8, 10)) - Number(timestampTarget.slice(8, 10));
-  const passedHour =
-    Number(now.slice(11, 13)) - Number(timestampTarget.slice(11, 13));
-  const passedMinute =
-    Number(now.slice(14, 16)) - Number(timestampTarget.slice(14, 16));
-  const passedSecond =
-    Number(now.slice(17, 19)) - Number(timestampTarget.slice(17, 19));
+  const passedSecond = Math.floor((now - timestampTarget) / 1000);
+  console.log(passedSecond);
+  const passedMinute = Math.floor(passedSecond / 60);
+  console.log(passedMinute);
+  const passedHour = Math.floor(passedMinute / 60);
+  console.log(passedHour);
+  const passedDay = Math.floor(passedHour / 24);
+  console.log(passedDay);
+  const passedMonth = Math.floor(passedDay / 30.4375);
+  console.log(passedMonth);
+  const passedYear = Math.floor(passedMonth / 12);
+  console.log(passedYear);
 
   if (passedYear > 0) {
     return `${passedYear}년 전`;
